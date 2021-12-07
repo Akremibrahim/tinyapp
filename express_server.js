be25af9a29c4e0c1bb1afcd4f01bcdd746ac563d
Akremibrahim <akrem.ibrahim@gmail.comgit config --global user.name Akrem>
2021-12-07 02:41:36 +0000

    shortening part 1 is completed

--- express_server.js
+++ express_server.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8081; // default port 
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2, 6);
+  };
+  console.log
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req, res) => {
+  app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
@@ -25,6 +31,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = {urls : urlDatabase };
   res.render("urls_index", templateVars)
@@ -33,4 +43,9 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");       
 });
